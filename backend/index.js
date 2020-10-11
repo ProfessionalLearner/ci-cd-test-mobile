1e0c1a9522da81d2584c2d4b39ebc7ecd97b7a0e
Arnur <arnur.nogaibekov@mail.utoronto.ca>
2020-10-11 23:33:44 +0600

    changes

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,17 @@
-const app = require('./app');
 const config = require('config')
 const connectDB = require('./database');
+
+const express = require('express')
+
+const app = express();
+
+app.use(express.json({extended: true}));
+
+//app.use('/api/words', require('./routes/words'));
+
+app.get('/', (req, res) => {
+    res.end('<h1>Server is running</h1>')
+});
 
 const PORT = config.get('port') || 5000;
 
